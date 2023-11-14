b8d808b12339755c7601ccf708b8dcd6044d380c
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2023-11-14 22:12:12 +0600

    Nav Bar Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router-dom"
+import NavBar from "./Components/NavBar/NavBar"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+ {/* <button className="relative overflow-hidden btn  border-indigo-500 group m-20">
+  <span className="relative z-10 transition-colors group-hover:text-white px-6 ">E Credit</span>
+  <span className="absolute inset-0 bg-indigo-500 transition-all duration-900 transform scale-x-0 origin-left group-hover:scale-x-50"></span>
+  <span className="absolute inset-0 bg-indigo-500 transition-all duration-900 transform scale-x-0 origin-right group-hover:scale-x-50"></span>
+</button> */}
+  <NavBar></NavBar>
+  <Outlet></Outlet>
     </>
   )
 }
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,52 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/Logo.png'
+import { FaRegUserCircle } from "react-icons/fa";
+const NavBar = () => {
+    const links = <>
+        <li><a>Products</a></li>
+        <li><a>Resources</a></li>
+        <li><a>Services</a></li>
+        <li><a>Insights</a></li>
+        <li><a>Consumer Support</a></li>
+        <li><a>About</a></li>
+        <li><a><FaRegUserCircle /> Member Login</a></li>
+    </>
+    return (
+        <div className="navbar shadow-xl py-4">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div className="drawer lg:hidden" style={{ zIndex: 10 }}>
+                        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                        <div className="drawer-content">
+                            <label htmlFor="my-drawer" className=" drawer-button"> <svg xmlns="http://www.w3.org/2000/svg" className="h-7" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg></label>
+                        </div>
+                        <div className="drawer-side">
+                            <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                                {links}
+
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+                <Link><img className='h-[50px] invisible lg:visible' src={logo} alt="" /></Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {
+                        links
+                    }
+                </ul>
+            </div>
+            <div className="navbar-end" style={{ zIndex: 1 }}>
+                <button className="relative overflow-hidden btn  border-indigo-500 group btn-sm">
+                    <span className="relative z-10 transition-colors group-hover:text-white px-6 ">Get Credit Monitoring</span>
+                    <span className="absolute inset-0 bg-indigo-500 transition-all duration-500 transform scale-x-0 origin-right group-hover:scale-x-100"></span>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;

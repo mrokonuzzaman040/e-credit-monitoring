5377454d826b18caa902e417ab1e0d01c29e6b36
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-11-17 16:39:46 +0600

    Add axios dependency to package.json

--- src/Components/Register/Step1.jsx
+++ src/Components/Register/Step1.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 import { MdBlock } from 'react-icons/md';
 
 const Step1 = () => {
+
+    const handelStep = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const first_name = form.first_name.value;
+        const last_name = form.last_name.value;
+        const dob = form.date.value;
+        const ssn = form.ssn.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+        const address = form.address.value;
+        const city = form.city.value;
+        const state = form.state.value;
+        const zip = form.zip.value;
+
+        const stepOneData = {
+            first_name,
+            last_name,
+            dob,
+            ssn,
+            email,
+            phone,
+            address,
+            city,
+            state,
+            zip
+        }
+
+        
+    }
+
     return (
         <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
             <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
@@ -19,26 +51,26 @@ const Step1 = () => {
                         <div className="w-full flex-1 mt-8">
                             <div className="mx-auto max-w-xs">
                                 <div className="">
-                                    <form className="mb-6">
+                                    <form onSubmit={handelStep} className="mb-6">
                                         <h2 className="text-xl font-semibold text-gray-700  mb-2">Tell us about yourself</h2>
                                         {/* name */}
                                         <div className="divider">Personal Info</div>
                                         <div className="grid grid-cols-2 gap-4">
                                             <div>
-                                                <label name="first_name" className="block text-gray-700  mb-1">First Name  <span className='text-red-500'>*</span></label>
-                                                <input required type="text" id="first_name" className="w-full rounded-lg border py-2 px-3" />
+                                                <label className="block text-gray-700  mb-1">First Name  <span className='text-red-500'>*</span></label>
+                                                <input required type="text" name="first_name" className="w-full rounded-lg border py-2 px-3" />
                                             </div>
                                             <div>
-                                                <label name="last_name" className="block text-gray-700  mb-1">Last Name <span className='text-red-500'>*</span></label>
-                                                <input required type="text" id="last_name" className="w-full rounded-lg border py-2 px-3" />
+                                                <label className="block text-gray-700  mb-1">Last Name <span className='text-red-500'>*</span></label>
+                                                <input required type="text" name="last_name" className="w-full rounded-lg border py-2 px-3" />
                                             </div>
                                         </div>
 
                                         {/* Date of birth */}
                                         <div className="grid grid-cols-1">
                                             <div>
-                                                <label name="first_name" className="block text-gray-700  mb-1">Month <span className='text-red-500'>*</span></label>
-                                                <input required type="date" id="first_name" className="w-full rounded-lg border py-2 px-3" />
+                                                <label className="block text-gray-700  mb-1">Month <span className='text-red-500'>*</span></label>
+                                                <input required type="date" name="date" className="w-full rounded-lg border py-2 px-3" />
                                             </div>
                                         </div>
 
@@ -47,8 +79,8 @@ const Step1 = () => {
                                             <h2 className='font-normal text-lg'>Last 4 Digits of Social Security Number <span className='text-red-500'>*</span></h2>
                                             <p className='font-normal text-sm'>By providing the last 4  digits of my SSN, I understand that TransUnion will retrieve my full SSN to deliver products that I input requiredrequest on this website.</p>
                                             <div className="grid grid-cols-2 justify-center">
-                                                <label name="city" className="block text-gray-700  mb-1"><MdBlock /></label>
-                                                <input required type="number" id="city" className="w-full rounded-lg border py-2 px-3" />
+                                                <label className="block text-gray-700  mb-1"><MdBlock /></label>
+                                                <input required type="number" name="ssn" className="w-full rounded-lg border py-2 px-3" />
                                             </div>
                                         </div>
 
@@ -58,35 +90,35 @@ const Step1 = () => {
                                         {/* Email address & phone */}
                                         <div>
                                             <div>
-                                                <label name="email" className="block text-gray-700  mb-1">Email Address <span className='text-red-500'>*</span></label>
-                                                <input required type="text" id="state" className="w-full rounded-lg border py-2 px-3" />
+                                                <label className="block text-gray-700  mb-1">Email Address <span className='text-red-500'>*</span></label>
+                                                <input required type="text" name="email" className="w-full rounded-lg border py-2 px-3" />
                                             </div>
                                             <div>
-                                                <label name="phone" className="block text-gray-700  mb-1">Phone  <span className='text-red-500'>*</span></label>
-                                                <input required type="text" id="zip" className="w-full rounded-lg border py-2 px-3" />
+                                                <label className="block text-gray-700  mb-1">Phone  <span className='text-red-500'>*</span></label>
+                                                <input required type="text" name="phone" className="w-full rounded-lg border py-2 px-3" />
                                             </div>
                                         </div>
 
                                         <div className="divider">Address Info</div>
                                         {/* Address */}
                                         <div className="mt-4">
-                                            <label name="address" className="block text-gray-700  mb-1">Address <span className='text-red-500'>*</span></label>
-                                            <input required type="text" id="address" className="w-full rounded-lg border py-2 px-3" />
+                                            <label className="block text-gray-700  mb-1">Address <span className='text-red-500'>*</span></label>
+                                            <input required type="text" name="address" className="w-full rounded-lg border py-2 px-3" />
                                         </div>
 
                                         <div className="mt-4">
-                                            <label name="city" className="block text-gray-700  mb-1">City <span className='text-red-500'>*</span></label>
-                                            <input required type="text" id="city" className="w-full rounded-lg border py-2 px-3" />
+                                            <label className="block text-gray-700  mb-1">City <span className='text-red-500'>*</span></label>
+                                            <input required type="text" name="city" className="w-full rounded-lg border py-2 px-3" />
                                         </div>
 
                                         <div className="grid grid-cols-2 gap-4 mt-4">
                                             <div>
-                                                <label name="state" className="block text-gray-700  mb-1">State <span className='text-red-500'>*</span></label>
-                                                <input required type="text" id="state" className="w-full rounded-lg border py-2 px-3" />
+                                                <label className="block text-gray-700  mb-1">State <span className='text-red-500'>*</span></label>
+                                                <input required type="text" name="state" className="w-full rounded-lg border py-2 px-3" />
                                             </div>
                                             <div>
-                                                <label name="zip" className="block text-gray-700  mb-1">ZIP Code <span className='text-red-500'>*</span></label>
-                                                <input required type="text" id="zip" className="w-full rounded-lg border py-2 px-3" />
+                                                <label className="block text-gray-700  mb-1">ZIP Code <span className='text-red-500'>*</span></label>
+                                                <input required type="text" name="zip" className="w-full rounded-lg border py-2 px-3" />
                                             </div>
                                         </div>
                                         {/* next btn */}

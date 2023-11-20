c765adc395c1634f1de2ae697768a3133f7a7c25
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-11-20 15:20:28 +0600

    Add AuthProvider and router import

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,51 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+import Home from '../Components/Home/Home';
+import Login from '../Components/Login/Login';
+import Register from '../Components/Register/Register';
+import Step2 from '../Components/Register/Step2';
+import Step3 from '../Components/Register/Step3';
+import Dashboard from '../LayOut/Dashboard/Dashboard';
+import App from "../App";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App></App>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+            {
+                path: 'login',
+                element: <Login></Login>
+            },
+            {
+                path: 'register',
+                element: <Register></Register>,
+            },
+            {
+                path: 'register/step2',
+                element: <Step2></Step2>
+            },
+            {
+                path: 'register/step3',
+                element: <Step3></Step3>
+            }
+        ]
+    },
+    {
+        path: "dashboard",
+        element: <Dashboard></Dashboard>,
+        children: [
+            {
+                path: 'dashboard',
+                element: <h2>Dashboard</h2>
+            },
+        ]
+    }
+]);
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import AuthProvider from './Provider/AuthProvider/AuthProvider';
 
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 
@@ -12,57 +12,10 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { router } from './Routes/Routes';
 
 const queryClient = new QueryClient();
 
-
-import App from './App';
-import Home from './Components/Home/Home';
-import Login from './Components/Login/Login';
-import Register from './Components/Register/Register';
-import AuthProvider from './Provider/AuthProvider/AuthProvider';
-import Step2 from './Components/Register/Step2';
-import Step3 from './Components/Register/Step3';
-import Dashboard from './LayOut/Dashboard/Dashboard';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App></App>,
-    children: [
-      {
-        path: "/",
-        element: <Home></Home>
-      },
-      {
-        path: 'login',
-        element: <Login></Login>
-      },
-      {
-        path: 'register',
-        element: <Register></Register>,
-      },
-      {
-        path: 'register/step2',
-        element: <Step2></Step2>
-      },
-      {
-        path: 'register/step3',
-        element: <Step3></Step3>
-      }
-    ]
-  },
-  {
-    path: "dashboard",
-    element: <Dashboard></Dashboard>,
-    children: [
-      {
-        path: 'dashboard',
-        element: <h2>Dashboard</h2>
-      },
-    ]
-  }
-]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>

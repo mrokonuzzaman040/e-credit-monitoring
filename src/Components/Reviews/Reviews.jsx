6370dd27cff052f0abddc94752768b61416a3633
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-01 21:42:36 +0600

    Refactor import paths in Reviews.jsx

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,12 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import useAxiosPublic from "../../Hooks/useAxiosPublic";
-import useAuth from "../../Hooks/useAuth";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import useAuth from "../../hooks/useAuth";
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 187eeb0de396efe2b24d344230efde8e2cf740f0
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -34,40 +30,10 @@ const Review = () => {
         , [axiosPublic]);
 
     return (
-<<<<<<< HEAD
-        <section className="my-20">
-            <Swiper className="mySwiper">
-                {reviews.map(review => (
-                    <SwiperSlide key={review._id}>
-                        <div className="flex flex-col justify-center items-center mx-24 my-16 gap-3">
-                            <div className="">
-                                <img className='w-[80px] h-[80px] rounded-full' src={review.image ? review.image : user} alt="" />
-                            </div>
-=======
-        <section>
-            <Swiper>
-                {reviews.map(review => (
-                    <SwiperSlide key={review._id}>
-                        <div className="flex flex-col items-center mx-24 my-16">
->>>>>>> 187eeb0de396efe2b24d344230efde8e2cf740f0
-                            <Rating
-                                style={{ maxWidth: 180 }}
-                                value={review.rating}
-                                readOnly
-                            />
-<<<<<<< HEAD
-                            <p className="py-8">{review.details}</p>
-                            <h3 className="text-2xl text-orange-400">{review.user_name ? review.user_name : 'User'}</h3>
-=======
-                            <img className='w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2' src={review.user_photo ? review.user_photo : user} alt="" />
-                            <p className="py-8">{review.details}</p>
-                            <h3 className="text-2xl text-orange-400">{review.user_name}</h3>
->>>>>>> 187eeb0de396efe2b24d344230efde8e2cf740f0
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </section>
+        <>
+            
+        </>
+
     );
 };
 

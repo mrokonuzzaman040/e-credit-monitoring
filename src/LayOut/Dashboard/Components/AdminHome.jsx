c0026e3e64940142712fbad3b7df31870a4ab69e
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-05 22:01:59 +0600

    Add react-chartjs-2 package and implement date filtering and chart display

--- src/LayOut/Dashboard/Components/AdminHome.jsx
+++ src/LayOut/Dashboard/Components/AdminHome.jsx
@@ -1,20 +1,52 @@
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
-
+import { Bar } from "react-chartjs-2";
 
 const AdminHome = () => {
-
     const axiosSecure = useAxiosSecure();
 
     const { data: users = [], refetch } = useQuery({
-        queryKey: ['users'],
+        queryKey: ["users"],
         queryFn: async () => {
-            const res = await axiosSecure.get('/users');
+            const res = await axiosSecure.get("/users");
             return res.data;
-        }
-    })
+        },
+    });
 
-    refetch();
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+
+    // Filter users by calendar
+    const filteredUsers = users.filter((user) => {
+        const userCalendar = new Date(user.calendar);
+        return userCalendar >= startDate && userCalendar <= endDate;
+    });
+
+    // Calculate total earnings and total profit based on filtered users
+    const totalEarnings = filteredUsers.reduce(
+        (total, users) => total + users.earnings,
+        0,
+    );
+    const totalProfit = filteredUsers.reduce(
+        (total, users) => total + users.profit,
+        0,
+    );
+
+
+    // refetch();
+    // Chart data
+    const chartData = {
+        labels: ["Total Earnings", "Total Profit"],
+        datasets: [
+            {
+                label: "Amount",
+                data: [totalEarnings, totalProfit],
+                backgroundColor: ["rgba(255, 99, 132, 0.6)", "rgba(54, 162, 235, 0.6)"],
+            },
+        ],
+    };
+
     return (
         <div>
             <div className="grid grid-cols-3 w-auto justify-center items-end text-center gap-4 ">
@@ -29,8 +61,8 @@ const AdminHome = () => {
                 <div className="bg-red-400 glass rounded-lg">
                     <div className="card text-neutral-content">
                         <div className="card-body items-center text-center">
-                            <h2 className="card-title">Toatal Earning</h2>
-                            <p>{ }</p>
+                            <h2 className="card-title">Total Earnings</h2>
+                            <p>{totalEarnings}</p>
                         </div>
                     </div>
                 </div>
@@ -38,10 +70,33 @@ const AdminHome = () => {
                     <div className="card text-neutral-content">
                         <div className="card-body items-center text-center">
                             <h2 className="card-title">Total Profit</h2>
-                            <p>{ }</p>
+                            <p>{totalProfit}</p>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="flex justify-center mt-4">
+                <div className="flex items-center mr-4">
+                    <label className="mr-2">Start Date:</label>
+                    <input
+                        type="date"
+                        value={startDate}
+                        onChange={(e) => setStartDate(e.target.value)}
+                        className="border border-gray-300 rounded px-2 py-1"
+                    />
+                </div>
+                <div className="flex items-center">
+                    <label className="mr-2">End Date:</label>
+                    <input
+                        type="date"
+                        value={endDate}
+                        onChange={(e) => setEndDate(e.target.value)}
+                        className="border border-gray-300 rounded px-2 py-1"
+                    />
+                </div>
+            </div>
+            <div className="chart-container">
+                {/* <Bar data={chartData} /> */}
             </div>
         </div>
     );

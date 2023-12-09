9943cfadd536d810e955bdd28a2cd45f8ab8037f
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-09 23:44:29 +0600

    Update Credit Score.

--- src/LayOut/User/Components/CreditSocre/CreditScore.jsx
+++ src/LayOut/User/Components/CreditSocre/CreditScore.jsx
@@ -1,9 +1,8 @@
 const CreditScore = () => {
-
-
     return (
-        <>
-        </>
+        <div>
+            <h1 className="text-center text-2xl ">Credit Score</h1>
+        </div>
     );
 };
 

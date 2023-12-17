6765f7d85e280288d519e30ee3fc2fe6bb62812a
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-17 20:41:35 +0600

    Refactor AdminHome component and update axios baseURL

--- src/LayOut/Dashboard/Components/AdminHome.jsx
+++ src/LayOut/Dashboard/Components/AdminHome.jsx
@@ -5,7 +5,6 @@ import { Bar } from "react-chartjs-2";
 
 const AdminHome = () => {
     const axiosSecure = useAxiosSecure();
-
     const { data: users = [], refetch } = useQuery({
         queryKey: ["users"],
         queryFn: async () => {
@@ -14,92 +13,77 @@ const AdminHome = () => {
         },
     });
 
-    const [startDate, setStartDate] = useState("");
-    const [endDate, setEndDate] = useState("");
+const totalEarnings = () => {
+    
+}
 
-    // Filter users by calendar
-    const filteredUsers = users.filter((user) => {
-        const userCalendar = new Date(user.calendar);
-        return userCalendar >= startDate && userCalendar <= endDate;
-    });
+const totalProfit = () => {
 
-    // Calculate total earnings and total profit based on filtered users
-    const totalEarnings = filteredUsers.reduce(
-        (total, users) => total + users.earnings,
-        0,
-    );
-    const totalProfit = filteredUsers.reduce(
-        (total, users) => total + users.profit,
-        0,
-    );
+}
 
 
-    // refetch();
-    // Chart data
-    const chartData = {
-        labels: ["Total Earnings", "Total Profit"],
-        datasets: [
-            {
-                label: "Amount",
-                data: [totalEarnings, totalProfit],
-                backgroundColor: ["rgba(255, 99, 132, 0.6)", "rgba(54, 162, 235, 0.6)"],
-            },
-        ],
-    };
+const [startDate, setStartDate] = useState("");
+const [endDate, setEndDate] = useState("");
 
-    return (
-        <div>
-            <div className="grid grid-cols-3 w-auto justify-center items-end text-center gap-4 ">
-                <div className="bg-red-400 glass rounded-lg">
-                    <div className="card text-neutral-content">
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">Total User</h2>
-                            <p>{users.length}</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="bg-red-400 glass rounded-lg">
-                    <div className="card text-neutral-content">
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">Total Earnings</h2>
-                            <p>{totalEarnings}</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="bg-red-400 glass rounded-lg">
-                    <div className="card text-neutral-content">
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">Total Profit</h2>
-                            <p>{totalProfit}</p>
-                        </div>
+// Filter users by calendar
+const filteredUsers = users.filter((user) => {
+    const userCalendar = new Date(user.calendar);
+    return userCalendar >= startDate && userCalendar <= endDate;
+});
+
+return (
+    <div>
+        <div className="grid grid-cols-3 w-auto justify-center items-end text-center gap-4 ">
+            <div className="bg-red-400 glass rounded-lg">
+                <div className="card text-neutral-content">
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Total User</h2>
+                        <p>{users.length}</p>
                     </div>
                 </div>
             </div>
-            <div className="flex justify-center mt-4">
-                <div className="flex items-center mr-4">
-                    <label className="mr-2">Start Date:</label>
-                    <input
-                        type="date"
-                        value={startDate}
-                        onChange={(e) => setStartDate(e.target.value)}
-                        className="border border-gray-300 rounded px-2 py-1"
-                    />
-                </div>
-                <div className="flex items-center">
-                    <label className="mr-2">End Date:</label>
-                    <input
-                        type="date"
-                        value={endDate}
-                        onChange={(e) => setEndDate(e.target.value)}
-                        className="border border-gray-300 rounded px-2 py-1"
-                    />
+            <div className="bg-red-400 glass rounded-lg">
+                <div className="card text-neutral-content">
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Total Earnings</h2>
+                        <p>{totalEarnings}</p>
+                    </div>
                 </div>
             </div>
-            <div className="chart-container">
-                {/* <Bar data={chartData} /> */}
+            <div className="bg-red-400 glass rounded-lg">
+                <div className="card text-neutral-content">
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Total Profit</h2>
+                        <p>{totalProfit}</p>
+                    </div>
+                </div>
             </div>
         </div>
-    );
+        <div className="flex justify-center mt-4">
+            <div className="flex items-center mr-4">
+                <label className="mr-2">Start Date:</label>
+                <input
+                    type="date"
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
+                    className="border border-gray-300 rounded px-2 py-1"
+                />
+            </div>
+            <div className="flex items-center">
+                <label className="mr-2">End Date:</label>
+                <input
+                    type="date"
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
+                    className="border border-gray-300 rounded px-2 py-1"
+                />
+            </div>
+        </div>
+        <div className="chart-container">
+
+        </div>
+    </div>
+);
 };
 
 export default AdminHome;

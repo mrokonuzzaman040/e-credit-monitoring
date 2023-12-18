d695d3128bf32663c7fc75c409e0216f78b3f1ac
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-19 01:21:28 +0600

    Update HTML and CSS styles in AddReview component

--- src/LayOut/Dashboard/Components/Review/AddReview.jsx
+++ src/LayOut/Dashboard/Components/Review/AddReview.jsx
@@ -32,7 +32,7 @@ const AddReview = () => {
         const name = data.name;
         const details = data.details;
         const rating = data.rating;
-        
+
         try {
             const res = await axiosPublic.post('/reviews', { name, details, rating, image });
             if (res.status === 200) {
@@ -72,16 +72,16 @@ const AddReview = () => {
             </div>
             <div className="divider"></div>
             <form onSubmit={handleSubmit(addReview)} className="">
-                <p className="py-4">Name: <input {...register('name', { required: true })} name='name' type="text" placeholder="Type here" className="input input-bordered w-full max-w-full" />
+                <p className="py-4 ">Name: <input className="bg-indigo-100 input input-bordered w-full max-w-full" {...register('name', { required: true })} name='name' type="text" placeholder="Type here" />
                 </p>
 
-                <p className="py-4">Details: <textarea {...register('details', { required: true })} name='details' type="text" placeholder="Type here" className="textarea textarea-bordered w-full max-w-full" />
+                <p className="py-4">Details: <textarea {...register('details', { required: true })} name='details' type="text" placeholder="Type here" className="textarea textarea-bordered w-full max-w-full bg-indigo-100" />
                 </p>
 
-                <p className="py-4">Rating: <input {...register('rating', { required: true })} name='rating' type="number" maxLength="1" placeholder="Type here" className="input input-bordered w-full max-w-full" />
+                <p className="py-4">Rating: <input {...register('rating', { required: true })} name='rating' type="number" maxLength="1" placeholder="Type here" className="input input-bordered w-full max-w-full bg-indigo-100" />
                 </p>
 
-                <p className="py-4"> Image: <input {...register('image',)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-indigo-100 focus:border-indigo-500" type="file" /> </p>
+                <p className="py-4"> Image: <input {...register('image',)} className="appearance-none block bg-indigo-100 w-full text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-indigo-100 focus:border-indigo-500" type="file" /> </p>
 
                 <div className="divider"></div>
                 <div className="flex justify-end">

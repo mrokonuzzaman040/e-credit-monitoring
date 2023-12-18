f5d568f13d7638b8f76fcbbf5279a2f8afc0a852
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-18 22:53:53 +0600

    Refactor Dashboard component layout

--- src/LayOut/Dashboard/Dashboard.jsx
+++ src/LayOut/Dashboard/Dashboard.jsx
@@ -49,8 +49,43 @@ const Dashboard = () => {
                         <label htmlFor="my-drawer-2" className="btn items-center justify-center btn-square btn-primary drawer-button"><FaAlignLeft ></FaAlignLeft></label>
                     </div>
                 </div>
-                <div className="mt-4 glass bg-indigo-300 min-h-full rounded-xl p-4">
+                {/* <div className="mt-4 glass bg-indigo-300 min-h-full rounded-xl p-4">
                     <Outlet></Outlet>
+                </div> */}
+
+                <div id="advatiser" class="relative py-28 lg:py-32 bg-white">
+                    <div className="relative z-0">
+                        <div class="mx-auto lg:max-w-7xl w-full px-5 sm:px-10 md:px-12 lg:px-5 flex flex-col gap-10 ">
+                            <div className="relative z-10">
+                                <Outlet />
+                            </div>
+                            <div class="absolute w-full lg:w-1/2 inset-y-0 lg:right-0 hidden lg:block">
+
+                                <span
+                                    class="absolute -left-6 md:left-4 top-24 lg:top-28 w-24 h-24 rotate-90 skew-x-12 rounded-3xl bg-green-400 blur-xl opacity-60 lg:opacity-95 lg:block hidden">
+                                </span>
+
+                                <span
+                                    class="absolute right-4 bottom-12 w-24 h-24 rounded-3xl bg-blue-600 blur-xl opacity-80">
+
+                                </span>
+
+                            </div>
+                            <span
+                                class="w-4/12 lg:w-2/12 aspect-square bg-gradient-to-tr from-blue-600 to-green-400 absolute -top-5 lg:left-0 rounded-full skew-y-12 blur-2xl opacity-40 skew-x-12 rotate-90">
+                            </span>
+                            <div class="relative flex flex-col items-center text-center lg:text-left lg:py-7 xl:py-8 
+                                lg:items-start lg:max-w-none max-w-3xl mx-auto lg:mx-0 lg:flex-1 lg:w-1/2">
+
+                                <div class="mt-10  w-full flex max-w-md mx-auto lg:mx-0">
+                                    <div class="flex sm:flex-row flex-col gap-5 w-full">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
 
             </div>
